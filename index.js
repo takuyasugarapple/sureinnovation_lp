53d83fb3d45066ebc37fd076c0cbfda2bfb30dad
takuyasugarapple <38278218+takuyasugarapple@users.noreply.github.com>
2018-09-12 11:18:02 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+$(function(){
+    $(".con-btn").click(function(){
+    var id = $(this).attr("href");
+    var position = $(id).offset().top;
+    
+    $("html, body").animate({
+      "scrollTop": position
+    },500);
+  });
+});
